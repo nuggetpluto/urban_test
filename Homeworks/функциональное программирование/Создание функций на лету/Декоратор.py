e057df1f9c2192eb27dfe2d2b5514cc12850dfb1
Eugeny <Hoperlite@gmail.com>
2024-07-10 10:42:12 +0300

    декоратор

---
+++ Homeworks/функциональное программирование/Создание функций на лету/Декоратор.py
@@ -0,0 +1,25 @@
+def is_prime(func):
+    def wrapper(a, b, c):
+        result = func(a, b, c)
+        if result > 1:
+            for i in range(2, int(result ** 0.5) + 1):
+                if result % i == 0:
+                    print("Составное")
+                    break
+            else:
+                print("Простое")
+        else:
+            print("Составное")
+        return result
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+# Пример использования:
+result = sum_three(2, 4, 6)
+print(result)
