5170b790d576f01a866c660facc53b2dd95a5885
nuggetpluto <korshikov1501@gmail.com>
2024-08-12 16:43:23 +0300

    тестики

---
+++ Homeworks/Юнит-тесты/test_runner.py
@@ -0,0 +1,26 @@
+import unittest
+from runner import Runner
+
+class TestRunner(unittest.TestCase):
+    def test_runner_walk(self):
+        runner = Runner('Иван')
+        for _ in range(10):
+            runner.walk()
+            self.assertEqual(runner.distance, 50)
+
+    def test_runner_run(self):
+        runner = Runner('Иван(run)')
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge_runner(self):
+        runner1 = Runner('Иван(challenge)')
+        runner2 = Runner('Петр(challenge)')
+        for i in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertEqual(runner1.distance, runner2.distance)
+if __name__ == '__main__':
+    unittest.main()
+
