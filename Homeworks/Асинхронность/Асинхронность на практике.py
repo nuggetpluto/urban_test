ba5265c1e617cf82d28fa4aa2153b907f050ffec
nuggetpluto <korshikov1501@gmail.com>
2024-08-20 12:48:51 +0300

    асинк

---
+++ Homeworks/Асинхронность/Асинхронность на практике.py
@@ -0,0 +1,19 @@
+import asyncio
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    for i in range(1, 6):  # От 1 до 5 включительно
+        print(f'Силач {name} поднял {i} шар.')
+        await asyncio.sleep(1 / power)
+    print(f'Силач {name} закончил соревнования.')
+
+
+async def start_tournament():
+    strongman1 = asyncio.create_task(start_strongman('мощнявый', 10))
+    strongman2 = asyncio.create_task(start_strongman('дохлый', 15))
+    strongman3 = asyncio.create_task(start_strongman('ну прям самый сильный', 12))
+    await asyncio.gather(strongman1, strongman2, strongman3)
+
+
+if __name__ == '__main__':
+    asyncio.run(start_tournament())
