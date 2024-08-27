1648cc8b22f63bea8eb13935eb2a8e02de0f93af
nuggetpluto <korshikov1501@gmail.com>
2024-08-27 14:39:19 +0300

    асинк

--- Homeworks/Асинхронность/бот.py
+++ Homeworks/Асинхронность/бот.py
@@ -1,21 +1,20 @@
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import asyncio
-
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
 from aiogram.types import Message
 
-api = '***'
-bot = Bot(token=api)
-dp = Dispatcher(bot, storage=MemoryStorage())
+TOKEN = '******************************** '
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
 # Обработка команды /start
 @dp.message_handler(commands=['start'])
 async def start(message: Message):
-    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот, помогающий твоему здоровью.')
 
 # Обработка всех остальных сообщений
 @dp.message_handler()
 async def all_messages(message: Message):
-    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
